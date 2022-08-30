c450f4caaa652624532f3be1488da6f1ac24e895
Suraj Gopal <surajcancode@gmail.com>
2022-08-30 10:49:43 -0400

    added scaffolding integration for db component of test app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles # creates links/routes for use
   root 'pages#home' # goes to the application_controller.rb route
   # GET request to about page
   get 'about', to: 'pages#about'
